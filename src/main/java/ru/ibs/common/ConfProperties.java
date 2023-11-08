9ff8d63bf43930a998626eb11193d5e87dbdd5f9
Vladislav Ustinov <wladustinov349@gmail.com>
2023-11-09 00:47:25 +0300

    Добавлен класс для чтения файлов properties Добавлен тестовый стенд

---
+++ src/main/java/ru/ibs/common/ConfProperties.java
@@ -0,0 +1,37 @@
+package ru.ibs.common;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfProperties {
+
+    private static FileInputStream fileInputStream;
+    private static Properties props;
+
+    static {
+        try {
+            fileInputStream = new FileInputStream("src/main/resources/environment.properties");
+            props = new Properties();
+            props.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static String getProperty(String key) {
+        return props.getProperty(key);
+    }
+
+    public static Object setProperty(String key, String value) {
+        return props.setProperty(key, value);
+    }
+}
